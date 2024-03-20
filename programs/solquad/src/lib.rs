7f7600cd865b52fbb38094285b7f824406f0ac00
haiquai <164226278+haiquai@users.noreply.github.com>
2024-03-21 00:50:07 +0700

    Update lib.rs

--- programs/solquad/src/lib.rs
+++ programs/solquad/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("5sFUqUTjAMJARrEafMX8f4J1LagdUQ9Y8TR8HwGNHkU8");
+declare_id!("Fqw1rkQExxV7ni9Y6sQytFG1XMnu4JMmGhBD1HzL87Df");
 
 #[program]
 pub mod solquad {
